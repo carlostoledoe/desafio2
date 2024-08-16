5d8396890b204bb988fb78f390208e14631ea2ec
Carlos Toledo <carlombo@hotmail.com>
2024-08-16 11:53:17 -0400

    Se configuró base de datos Postgres

--- desafio2/settings.py
+++ desafio2/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'desafio2.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'desafio2',
+        'USER': 'lombo', # postgres por defecto
+        'PASSWORD': '1234',
+        'HOST': '127.0.0.1',
+        'PORT': '5432'
     }
 }
 
