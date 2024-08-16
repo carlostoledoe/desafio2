9922a5332feb91d69534f7d15fbd06fdb20314b9
Carlos Toledo <carlombo@hotmail.com>
2024-08-16 12:19:24 -0400

    Se crearon modelos Tarea y Subtarea

--- desafioadl/models.py
+++ desafioadl/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Tarea(models.Model):
+    descripcion = models.TextField(default='')
+    eliminada = models.BooleanField(default=False)
+
+class SubTarea(models.Model):
+    descripcion = models.TextField(default='')
+    eliminada = models.BooleanField(default=False)
+    tarea = models.ForeignKey(Tarea, related_name='subtarea', on_delete=models.CASCADE )
